f0510a53f5e5a1cc5cfad674606132793029025f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-29 17:13:18 +0900

    Create round.ts

---
+++ round.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+
+const filePath: string = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input: string = fs.readFileSync(filePath).toString().split("\n")[0];
+
+const X: number = parseFloat(input);
+const a: number = parseInt((X * 10).toString()); // ここで文字列に変換
+
+if (a % 10 >= 5) {
+  console.log(parseInt(X.toString()) + 1);
+} else if (a % 10 <= 4) {
+  console.log(parseInt(X.toString()));
+}
