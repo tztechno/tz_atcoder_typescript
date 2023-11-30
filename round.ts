adbf745e10159637172aad26c07de50d5d0487dc
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-11-30 13:13:41 +0900

    Update round.ts

--- round.ts
+++ round.ts
@@ -1,5 +1,4 @@
 import * as fs from "fs";
-
 const filePath: string = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const input: string = fs.readFileSync(filePath).toString().split("\n")[0];
 
