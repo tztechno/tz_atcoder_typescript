1e434719ca4a4ccba3c1f531a9c34fcdc5ffbd6c
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-13 13:22:26 +0900

    Update magnitude.ts

--- magnitude.ts
+++ magnitude.ts
@@ -1,5 +1,4 @@
 import * as fs from 'fs';
-
 const filePath: string = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input: string = fs.readFileSync(filePath, 'utf-8').split('\n')[0];
 
