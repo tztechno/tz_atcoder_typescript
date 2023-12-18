38ce4f218763af4ac3ca711a01fcc152847738f1
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-18 12:29:17 +0900

    Update determinant.ts

--- determinant.ts
+++ determinant.ts
@@ -1,7 +1,10 @@
+
+### error const input: string = fs.readFileSync(filePath).toString().split("\n")[0]; ### not for multiple input values
+    
 import * as fs from "fs";
 const filePath: string = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
-const S: string = fs.readFileSync(filePath).toString().split("\n")[0];
-    
+const input: string[] = fs.readFileSync(filePath).toString().split("\n");
+
 const [a, b] = input[0].split(' ').map(Number);
 const [c, d] = input[1].split(' ').map(Number);
 
