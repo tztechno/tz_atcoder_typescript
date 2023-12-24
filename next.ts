c82bcb24ca8c90e75e91b4537151e6a764e8181b
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-24 21:44:56 +0900

    Update next.ts

--- next.ts
+++ next.ts
@@ -1,13 +1,20 @@
 #ABC329_B
 #next
 
-
 import * as fs from 'fs';
 const filePath: string = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
 const input: string = fs.readFileSync(filePath, 'utf-8').split('\n')[0];
     
+const C = Array.from(new Set(input.split(' ').map(Number))).sort((a, b) => b - a);
+console.log(C[1]);    
+
+###############
+
+import * as fs from "fs";
+const filePath: string = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+const input: string = fs.readFileSync(filePath).toString().split("\n")[1];    
+    
 const A = input.split(' ').map(Number);
 const B = new Set(A);
 const C = Array.from(B).sort((a, b) => b - a);
-
-console.log(C[1]);   
+console.log(C[1]);    
