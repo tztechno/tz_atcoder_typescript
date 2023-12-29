ada64e4d1a9340e598d4a95f46597591a3aaad8f
stpeteishii <47345279+tztechno@users.noreply.github.com>
2023-12-29 19:06:02 +0900

    Update plural.ts

--- plural.ts
+++ plural.ts
@@ -1,6 +1,8 @@
 //ABC179_A
 //plural
 
+/////////////////////////
+
 import * as fs from "fs";
 const filePath: string = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
 const S: string = fs.readFileSync(filePath, "utf-8").split("\n")[0];
@@ -12,3 +14,14 @@ if (S[length-1]=="s") {
 else {
   console.log(S+"s") 
 }
+
+
+/////////////////////////
+
+import * as fs from 'fs';
+const S: string = fs.readFileSync('/dev/stdin', 'utf8').trim();
+
+console.log(s.substring(S.length-1) === 's' ? s + 'es' : s + 's');
+
+
+/////////////////////////
